d0752a620d049d5b60ff0221dcd751b59992f074
aliriza <aliriza.keskin@pardus.org.tr>
2023-03-26 13:55:52 +0000

    fix migrate

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -2,6 +2,6 @@
 
 # migrate ubuntu to linuxmint
 apt update --allow-insecure-repositories
-apt install linuxmint-keyring -y --allow-unauthenticated
+apt install linuxmint-keyring -y --allow-unauthenticated -o Dpkg::Options::="--force-confnew" -yq --force-yes
 apt update
 apt full-upgrade -y -o Dpkg::Options::="--force-confnew" -yq --force-yes
