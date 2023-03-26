40c24c50743795a46f753c8252f09f6afc41a92d
sulincix <aliriza.keskin@pardus.org.tr>
2023-03-26 15:54:28 +0000

    Update migrate.sh

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -5,3 +5,5 @@ apt update --allow-insecure-repositories
 apt install linuxmint-keyring -y --allow-unauthenticated -o Dpkg::Options::="--force-confnew" -yq --force-yes
 apt update
 apt full-upgrade -y -o Dpkg::Options::="--force-confnew" -yq --force-yes
+# fix networkmanager unmanaged issue
+touch /etc/NetworkManager/conf.d/10-globally-managed-devices.conf
