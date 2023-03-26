b2eada06b13e973b903951e73ed0ce7722a8d358
aliriza <aliriza.keskin@pardus.org.tr>
2023-03-26 13:48:32 +0000

    fix migrate

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -2,5 +2,6 @@
 
 # migrate ubuntu to linuxmint
 apt update --allow-insecure-repositories
-apt install linuxmint-keyring -y
+apt install linuxmint-keyring -y --allow-unauthenticated
+apt update
 apt full-upgrade -y
